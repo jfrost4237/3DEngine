9bd9b14a7364b0c3d65f152870cedfad6e3f945e
piotr <piotr@DESKTOP-B0J4D1E>
2023-08-29 12:41:14 +0200

    backup

--- 3DEngine/Graphics.h
+++ 3DEngine/Graphics.h
@@ -3,6 +3,7 @@
 #include "ChiliException.h"
 #include <d3d11.h>
 #include <vector>
+#include <wrl.h>
 #include "DxgiInfoManager.h"
 
 class Graphics
@@ -38,15 +39,19 @@ public:
 	Graphics(HWND hWnd);
 	Graphics(const Graphics&) = delete;
 	Graphics& operator=(const Graphics&) = delete;
-	~Graphics();
+	~Graphics() = default;
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
+	void DrawTestTriangle() {
+		//pContext->IAGetVertexBuffers();
+		pContext->Draw(3u,0u);
+	}
 private:
 #ifndef NDEBUG
 	DxgiInfoManager infoManager;
 #endif
-	ID3D11Device* pDevice = nullptr;
-	IDXGISwapChain* pSwap = nullptr;
-	ID3D11DeviceContext* pContext = nullptr;
-	ID3D11RenderTargetView* pTarget = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11Device> pDevice;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> pSwap;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> pContext;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> pTarget;
 };
