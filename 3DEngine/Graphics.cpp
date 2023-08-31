f34958c1ead4e992589f533bd3ec3c9a28b5d720
piotr <piotr@DESKTOP-B0J4D1E>
2023-09-01 00:22:21 +0200

    spinning matrix

--- 3DEngine/Graphics.cpp
+++ 3DEngine/Graphics.cpp
@@ -100,7 +100,7 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	pContext->ClearRenderTargetView(pTarget.Get(), color);
 }
 
-void Graphics::DrawTestTriangle() {
+void Graphics::DrawTestTriangle(float angle) {
 	namespace wrl = Microsoft::WRL;
 	HRESULT hr;
 
@@ -170,6 +170,38 @@ void Graphics::DrawTestTriangle() {
 	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
 
 	pContext->IASetIndexBuffer(pIndexBuffer.Get(),DXGI_FORMAT_R16_UINT,0u);
+
+	//create constant buff
+	struct ConstantBuffer {
+		struct {
+			float element[4][4];
+		} transformation;
+	};
+
+	const ConstantBuffer cb =
+	{
+		{
+			(3.0f/4.0f)*std::cos(angle), std::sin(angle),0.0f,0.0f,
+			(3.0f / 4.0f) * -std::sin(angle), std::cos(angle),0.0f,0.0f,
+			0.0f,0.0f,1.0f,0.0f,
+			0.0f,0.0f,0.0f,1.0f
+		}
+	};
+	wrl::ComPtr<ID3D11Buffer> pConstantBuffer;
+	D3D11_BUFFER_DESC cbd;
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.Usage = D3D11_USAGE_DYNAMIC;
+	cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cbd.MiscFlags = 0u;
+	cbd.ByteWidth = sizeof(cb);
+	cbd.StructureByteStride = 0u;
+	D3D11_SUBRESOURCE_DATA csd = {};
+	csd.pSysMem = &cb;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&cbd, &csd, &pConstantBuffer));
+
+	//binding const buff -> Vrtx shader
+
+	pContext->VSSetConstantBuffers(0u,1u,pConstantBuffer.GetAddressOf());
 
 	// create pixel shader
 	wrl::ComPtr<ID3D11VertexShader> pVertexShader;
